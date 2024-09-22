4150b04fb989f1658c484f580365d346f39ac202
lou1s vuong <lou1svuong.dev@gmail.com>
2024-09-22 20:46:57 +0700

    feat: Add AuthLayout component with global CSS styles and temporary logo
    This commit adds a new component called AuthLayout to the (auth) directory. The AuthLayout component is responsible for rendering the layout of the authentication pages. It includes global CSS styles and a temporary logo component. This component is designed to be used as a wrapper around the content of the authentication pages.
    
    The AuthLayout component is a flex container that centers its children vertically and horizontally. It has a responsive layout with a grid system for different screen sizes. The left side of the layout contains a sidebar with a background color and a logo. The right side of the layout contains the actual content of the authentication pages.
    
    The logo component is a temporary placeholder and will be replaced with the actual logo in a future commit.
    
    This commit is a feature addition and improves the overall user experience of the authentication pages.

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import TempLogo from '../../../public/temp-logo'
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <div className='container relative h-full flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-[40%_60%] px-16 lg:px-0'>
+        <div className='relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex '>
+          <div className='absolute inset-0 bg-zinc-900' />
+          <div className='relative z-20 flex items-center text-lg font-medium'>
+            <Link href='/'>
+              <TempLogo />
+            </Link>
+          </div>
+          <div className='relative z-20 mt-auto'>
+            <blockquote className='space-y-2'>
+              <p className='text-lg'>&ldquo;The resource management platform to plan your team's best work&rdquo;</p>
+              <footer className='text-sm'>Sofia Davis</footer>
+            </blockquote>
+          </div>
+        </div>
+        {children}
+      </div>
+    </div>
+  )
+}
