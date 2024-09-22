749195739f459da1025fa468af71904fc33b8f06
lou1s vuong <lou1svuong.dev@gmail.com>
2024-09-22 20:46:49 +0700

    feat: Add layout component with global CSS styles and fonts

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+import './globals.css'
+import { Header } from '@/components/ui/header'
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900'
+})
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900'
+})
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | NOLIMIT',
+    default: 'NOLIMIT'
+  },
+  description: "The resource management platform to plan your team's best work"
+}
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang='en'>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#ffffff]`}>{children}</body>
+    </html>
+  )
+}
