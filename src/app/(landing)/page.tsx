648d660f89f6a42656e623bbe7fa658bd1ce8aa1
lou1s vuong <lou1svuong.dev@gmail.com>
2024-09-22 20:47:35 +0700

    feat: Add HomeLayout component with banner
    This commit adds the HomeLayout component to the landing page. The component includes a banner section that is displayed at the top of the page.

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+import Banner from '@/containers/landingpage/banner'
+
+export default function Home() {
+  return (
+    <div className='flex-col justify-center items-center h-screen'>
+      <Banner />
+    </div>
+  )
+}
