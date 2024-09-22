ddc31845411b00f0613dfc90868d47c1453b25a4
lou1s vuong <lou1svuong.dev@gmail.com>
2024-09-22 20:46:37 +0700

    chore : Add some icons for features in nav

---
+++ public/logo-navbar/planning-navbar.tsx
@@ -0,0 +1,178 @@
+import React from 'react'
+
+function PlaningIcon() {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='107' height='141' fill='none'>
+      <rect
+        width='98.008'
+        height='130.132'
+        x='7.858'
+        y='10.467'
+        fill='#fff'
+        stroke='#242C39'
+        strokeWidth='0.8'
+        rx='3.6'
+      ></rect>
+      <rect
+        width='98.008'
+        height='130.132'
+        x='0.4'
+        y='10.467'
+        fill='#fff'
+        stroke='#242C39'
+        strokeWidth='0.8'
+        rx='3.6'
+      ></rect>
+      <rect
+        width='6.2'
+        height='3.2'
+        x='37.4'
+        y='58.4'
+        fill='#FEF2A4'
+        stroke='#242C39'
+        strokeWidth='0.8'
+        rx='1.6'
+      ></rect>
+      <rect
+        width='6.2'
+        height='3.2'
+        x='49.4'
+        y='58.4'
+        fill='#FEF2A4'
+        stroke='#242C39'
+        strokeWidth='0.8'
+        rx='1.6'
+      ></rect>
+      <rect
+        width='6.2'
+        height='3.2'
+        x='61.4'
+        y='58.4'
+        fill='#FEF2A4'
+        stroke='#242C39'
+        strokeWidth='0.8'
+        rx='1.6'
+      ></rect>
+      <rect
+        width='6.2'
+        height='3.2'
+        x='73.4'
+        y='58.4'
+        fill='#FEF2A4'
+        stroke='#242C39'
+        strokeWidth='0.8'
+        rx='1.6'
+      ></rect>
+      <rect
+        width='13.2'
+        height='2.2'
+        x='15.4'
+        y='32.4'
+        fill='#F5D90A'
+        stroke='#242C39'
+        strokeWidth='0.8'
+        rx='1.1'
+      ></rect>
+      <rect
+        width='13.2'
+        height='2.2'
+        x='15.4'
+        y='41.4'
+        fill='#F5D90A'
+        stroke='#242C39'
+        strokeWidth='0.8'
+        rx='1.1'
+      ></rect>
+      <rect
+        width='13.2'
+        height='2.2'
+        x='15.4'
+        y='50.4'
+        fill='#F5D90A'
+        stroke='#242C39'
+        strokeWidth='0.8'
+        rx='1.1'
+      ></rect>
+      <path
+        fill='url(#a)'
+        d='M44.542 46.596c-7.871 0-4.708-10.565-11.542-10.565V54h51V36.816c-5.047 0-5.368 6.945-10.468 6.944-7.248 0-5.1-12.76-13.153-12.76-7.708 0-7.784 15.596-15.837 15.596z'
+      ></path>
+      <path
+        stroke='#242C39'
+        strokeLinecap='round'
+        strokeWidth='0.8'
+        d='M33 35.839C40.067 36.176 36.67 46 44.542 46c8.053 0 8.13-15 15.837-15 8.053 0 5.905 12.273 13.153 12.273 5.1 0 5.421-6.679 10.468-6.679'
+      ></path>
+      <path stroke='#242C39' strokeLinecap='round' strokeWidth='0.8' d='M33 26v27.557c0 .245.198.443.443.443H84'></path>
+      <rect
+        width='20.826'
+        height='8.894'
+        x='40.669'
+        y='6.739'
+        fill='#CEE7FE'
+        stroke='#242C39'
+        strokeWidth='0.8'
+        rx='1.837'
+      ></rect>
+      <path fill='#CEE7FE' stroke='#242C39' d='M38.532.5h30.075c.96 0 1.737.778 1.737 1.737V9.94H38.532V.5z'></path>
+      <rect
+        width='32.012'
+        height='11.877'
+        x='35.076'
+        y='0.4'
+        fill='#CEE7FE'
+        stroke='#242C39'
+        strokeWidth='0.8'
+        rx='1.837'
+      ></rect>
+      <circle cx='21.117' cy='82.054' r='5.8' fill='#fff' stroke='#242C39' strokeWidth='0.8'></circle>
+      <circle cx='21.117' cy='103.992' r='5.8' fill='#fff' stroke='#242C39' strokeWidth='0.8'></circle>
+      <circle cx='21.117' cy='125.929' r='5.8' fill='#fff' stroke='#242C39' strokeWidth='0.8'></circle>
+      <rect
+        width='50.223'
+        height='3.735'
+        x='33.46'
+        y='79.091'
+        fill='#fff'
+        stroke='#242C39'
+        strokeWidth='0.8'
+        rx='1.868'
+      ></rect>
+      <rect
+        width='50.223'
+        height='3.735'
+        x='33.46'
+        y='101.768'
+        fill='#fff'
+        stroke='#242C39'
+        strokeWidth='0.8'
+        rx='1.868'
+      ></rect>
+      <rect
+        width='50.223'
+        height='3.735'
+        x='33.46'
+        y='123.311'
+        fill='#fff'
+        stroke='#242C39'
+        strokeWidth='0.8'
+        rx='1.868'
+      ></rect>
+      <path
+        stroke='#000'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='0.8'
+        d='M17.896 81.752l2.819 2.818c0-2.818 3.287-10.333 10.332-12.681m-13.151 31.468l2.819 2.818c0-2.818 3.287-10.332 10.332-12.68m-13.151 32.407l2.819 2.818c0-2.818 3.287-10.333 10.332-12.681'
+      ></path>
+      <defs>
+        <linearGradient id='a' x1='58.692' x2='58.692' y1='23.675' y2='57.014' gradientUnits='userSpaceOnUse'>
+          <stop stopColor='#78B4EB'></stop>
+          <stop offset='0.938' stopColor='#fff'></stop>
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+
+export default PlaningIcon
---
+++ public/logo-navbar/realtime-chatting-navbar.tsx
@@ -0,0 +1,258 @@
+import React from 'react'
+
+function RealtimeChattingIcon() {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='137' height='134' fill='none'>
+      <path fill='#147D6F' stroke='#242C39' strokeWidth='0.531' d='M74.454 10.323H63.92v11.539h10.534z'></path>
+      <rect
+        width='20.592'
+        height='13.551'
+        x='-0.265'
+        y='0.265'
+        fill='#40C4AA'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='2.102'
+        transform='matrix(-1 0 0 1 79.218 0)'
+      ></rect>
+      <path
+        fill='#147D6F'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        d='M111.756 32.17l-3.827-3.955-5.402 5.226 3.827 3.955z'
+      ></path>
+      <path
+        fill='#A5E4D4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        d='M114.743 34.88a6.776 6.776 0 00-9.422-9.74l-1.958 1.895a.028.028 0 00-.001.039l9.384 9.7a.028.028 0 00.039 0l1.958-1.894z'
+      ></path>
+      <path
+        fill='#147D6F'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        d='M25.102 32.23l3.827-3.956L34.33 33.5l-3.827 3.956z'
+      ></path>
+      <path
+        fill='#A5E4D4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        d='M22.115 34.94a6.775 6.775 0 119.422-9.74l1.959 1.895c.01.01.01.028 0 .039l-9.384 9.7a.028.028 0 01-.039 0l-1.958-1.895z'
+      ></path>
+      <circle
+        r='52.542'
+        fill='#40C4AA'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        transform='matrix(-1 0 0 1 68.185 72.924)'
+      ></circle>
+      <circle
+        r='46.507'
+        fill='#fff'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        transform='matrix(-1 0 0 1 68.182 72.923)'
+      ></circle>
+      <rect
+        width='1.481'
+        height='5.504'
+        x='-0.265'
+        y='0.265'
+        fill='#C4C4C4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='0.524'
+        transform='matrix(-1 0 0 1 69.163 30.177)'
+      ></rect>
+      <rect
+        width='1.481'
+        height='5.504'
+        x='-0.265'
+        y='0.265'
+        fill='#C4C4C4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='0.524'
+        transform='matrix(-1 0 0 1 69.163 109.637)'
+      ></rect>
+      <rect
+        width='1.481'
+        height='5.504'
+        x='-0.265'
+        y='-0.265'
+        fill='#C4C4C4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='0.524'
+        transform='matrix(0 -1 -1 0 110.408 73.397)'
+      ></rect>
+      <rect
+        width='1.481'
+        height='5.504'
+        x='-0.265'
+        y='-0.265'
+        fill='#C4C4C4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='0.524'
+        transform='matrix(0 -1 -1 0 30.944 73.398)'
+      ></rect>
+      <rect
+        width='1.481'
+        height='5.504'
+        x='-0.35'
+        y='0.135'
+        fill='#C4C4C4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='0.524'
+        transform='scale(-1 1) rotate(-24 37.648 220.273)'
+      ></rect>
+      <rect
+        width='1.481'
+        height='5.504'
+        x='-0.35'
+        y='0.135'
+        fill='#C4C4C4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='0.524'
+        transform='scale(-1 1) rotate(-24 224.567 180.536)'
+      ></rect>
+      <rect
+        width='1.481'
+        height='5.504'
+        x='-0.135'
+        y='-0.35'
+        fill='#C4C4C4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='0.524'
+        transform='scale(1 -1) rotate(66 122.983 36.64)'
+      ></rect>
+      <rect
+        width='1.481'
+        height='5.504'
+        x='-0.135'
+        y='-0.35'
+        fill='#C4C4C4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='0.524'
+        transform='scale(1 -1) rotate(66 61.805 -3.09)'
+      ></rect>
+      <rect
+        width='1.481'
+        height='5.504'
+        x='-0.346'
+        y='-0.147'
+        fill='#C4C4C4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='0.524'
+        transform='scale(-1 1) rotate(-68 -10.2 108.686)'
+      ></rect>
+      <rect
+        width='1.481'
+        height='5.504'
+        x='-0.346'
+        y='-0.147'
+        fill='#C4C4C4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='0.524'
+        transform='scale(-1 1) rotate(-68 48.703 68.954)'
+      ></rect>
+      <rect
+        width='1.481'
+        height='5.504'
+        x='0.147'
+        y='-0.346'
+        fill='#C4C4C4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='0.524'
+        transform='scale(1 -1) rotate(22 331.783 155.818)'
+      ></rect>
+      <rect
+        width='1.481'
+        height='5.504'
+        x='0.147'
+        y='-0.346'
+        fill='#C4C4C4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='0.524'
+        transform='scale(1 -1) rotate(22 127.384 116.088)'
+      ></rect>
+      <rect
+        width='1.481'
+        height='5.504'
+        x='-0.375'
+        fill='#C4C4C4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='0.524'
+        transform='scale(-1 1) rotate(-45 3.086 141.059)'
+      ></rect>
+      <rect
+        width='1.481'
+        height='5.504'
+        x='-0.375'
+        fill='#C4C4C4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='0.524'
+        transform='scale(-1 1) rotate(-45 99.005 101.33)'
+      ></rect>
+      <rect
+        width='1.481'
+        height='5.504'
+        y='-0.375'
+        fill='#C4C4C4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='0.524'
+        transform='scale(1 -1) rotate(45 173.316 66.006)'
+      ></rect>
+      <rect
+        width='1.481'
+        height='5.504'
+        y='-0.375'
+        fill='#C4C4C4'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='0.524'
+        transform='scale(1 -1) rotate(45 77.396 26.277)'
+      ></rect>
+      <rect
+        width='53.785'
+        height='4.498'
+        x='-0.265'
+        y='0.265'
+        fill='#254CBA'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        rx='1.707'
+        transform='matrix(-1 0 0 1 79.22 70.408)'
+      ></rect>
+      <circle
+        r='6.273'
+        fill='#D0E8FF'
+        stroke='#242C39'
+        strokeWidth='0.531'
+        transform='matrix(-1 0 0 1 68.183 72.924)'
+      ></circle>
+      <path
+        stroke='#147D6F'
+        strokeDasharray='2.94 2.94'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='1.468'
+        d='M9.083 30.157c-1.133-3.286 0-9.858 3.74-13.259 6.64-6.038 13.599-3.4 16.998-2.04m-28.478 7.2c-1.133-3.287.6-9.238 4.34-12.638 6.64-6.038 12.999-4.021 16.398-2.661m105.653 23.089c1.134-3.287 0-9.859-3.739-13.26-6.641-6.037-13.599-3.4-16.999-2.04m28.478 7.2c1.134-3.286-.6-9.237-4.339-12.638-6.641-6.038-12.999-4.021-16.399-2.661'
+      ></path>
+    </svg>
+  )
+}
+
+export default RealtimeChattingIcon
