aaae158f75c6b4be1a14b2db139f11ea1deb25e5
Ken Ny <kennynguyen1631984@gmail.com>
2024-09-24 16:27:41 +0700

    refactor: Update layout to include Badge component for mobile view
    refactor: Update sidebar icons in SidebarDashboard and SidebarLink components
    
    refactor: Update layout to conditionally render children based on screen size
    
    feat: Add nextjs-toploader package
    
    feat: Add new pages and components

--- src/app/dashboard/message/page.tsx
+++ src/app/dashboard/message/page.tsx
@@ -1,8 +1,37 @@
+// export default function MessagePage() {
+//   return (
+//     <div className='flex flex-col justify-center items-center h-screen'>
+//       <div>Message Page</div>
+//       <div>Is Updating</div>
+//     </div>
+//   )
+// }
+
+import { cookies } from 'next/headers'
+import Image from 'next/image'
+
+import { Mail } from '@/containers/message/mail'
+
+import { accounts, mails } from '@/app/dashboard/message/data'
+
 export default function MessagePage() {
+  const layout = cookies().get('react-resizable-panels:layout:mail')
+  const collapsed = cookies().get('react-resizable-panels:collapsed')
+
+  const defaultLayout = layout ? JSON.parse(layout.value) : undefined
+  const defaultCollapsed = collapsed ? JSON.parse(collapsed.value) : undefined
+
   return (
-    <div className='flex flex-col justify-center items-center h-screen'>
-      <div>Message Page</div>
-      <div>Is Updating</div>
-    </div>
+    <>
+      <div className='flex-col h-screen'>
+        <Mail
+          accounts={accounts}
+          mails={mails}
+          defaultLayout={defaultLayout}
+          defaultCollapsed={defaultCollapsed}
+          navCollapsedSize={4}
+        />
+      </div>
+    </>
   )
 }
---
+++ src/containers/message/mail.tsx
@@ -0,0 +1,91 @@
+'use client'
+
+import * as React from 'react'
+import {
+  AlertCircle,
+  Archive,
+  ArchiveX,
+  File,
+  Inbox,
+  MessagesSquare,
+  Search,
+  Send,
+  ShoppingCart,
+  Trash2,
+  Users2
+} from 'lucide-react'
+
+import { cn } from '@/lib/utils'
+import { Separator } from '@/components/ui/separator'
+import { Input } from '@/components/ui/input'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { TooltipProvider } from '@/components/ui/tooltip'
+import { MailDisplay } from '@/containers/message/mail-display'
+import { MailList } from '@/containers/message/mail-list'
+import { type Mail } from '@/app/dashboard/message/data'
+import { useMail } from '@/hooks/use-mail'
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable'
+
+interface MailProps {
+  accounts: {
+    label: string
+    email: string
+    icon: React.ReactNode
+  }[]
+  mails: Mail[]
+  defaultLayout: number[] | undefined
+  defaultCollapsed?: boolean
+  navCollapsedSize: number
+}
+
+export function Mail({
+  accounts,
+  mails,
+  defaultLayout = [20, 75],
+  defaultCollapsed = false,
+  navCollapsedSize
+}: MailProps) {
+  const [isCollapsed, setIsCollapsed] = React.useState(defaultCollapsed)
+  const [mail] = useMail()
+
+  return (
+    <TooltipProvider delayDuration={0}>
+      <ResizablePanelGroup direction='horizontal' className='h-full items-stretch'>
+        <ResizablePanel defaultSize={20} minSize={20} maxSize={35} collapsedSize={navCollapsedSize} collapsible={true}>
+          <Tabs defaultValue='all'>
+            <div className='flex items-center px-4 py-2'>
+              <h1 className='text-xl font-bold'>Inbox</h1>
+              <TabsList className='ml-auto'>
+                <TabsTrigger value='all' className='text-zinc-600 dark:text-zinc-200'>
+                  All mail
+                </TabsTrigger>
+                <TabsTrigger value='unread' className='text-zinc-600 dark:text-zinc-200'>
+                  Unread
+                </TabsTrigger>
+              </TabsList>
+            </div>
+            <Separator />
+            <div className='bg-background/95 p-4 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
+              <form>
+                <div className='relative'>
+                  <Search className='absolute left-2 top-2.5 h-4 w-4 text-muted-foreground' />
+                  <Input placeholder='Search' className='pl-8' />
+                </div>
+              </form>
+            </div>
+            <TabsContent value='all' className='m-0'>
+              <MailList items={mails} />
+            </TabsContent>
+            <TabsContent value='unread' className='m-0'>
+              <MailList items={mails.filter((item) => !item.read)} />
+            </TabsContent>
+          </Tabs>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={80} maxSize={80}>
+          <MailDisplay mail={mails.find((item) => item.id === mail.selected) || null} />
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </TooltipProvider>
+  )
+}
