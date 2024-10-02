72b2750863bb49d0fb04f9e6449f68baa3d0cdec
Ken Ny <kennynguyen1631984@gmail.com>
2024-10-03 01:09:26 +0700

    feat: Add authApiRequest module for handling authentication API requests

---
+++ src/apiRequests/auth.ts
@@ -0,0 +1,60 @@
+import http from '@/lib/http'
+import {
+  LoginBodyType,
+  LoginResType,
+  LogoutBodyType,
+  RefreshTokenBodyType,
+  RefreshTokenResType,
+  RegisterBodyType,
+  RegisterResType
+} from '@/schemaValidations/auth.schema'
+
+const authApiRequest = {
+  refreshTokenRequest: null as Promise<{
+    status: number
+    payload: RefreshTokenResType
+  }> | null,
+  sRegister: (body: RegisterBodyType) => http.post<RegisterResType>('/api/v1/user/register', body),
+  register: (body: RegisterBodyType) =>
+    http.post<RegisterResType>('/api/auth/register', body, {
+      baseUrl: ''
+    }),
+  sLogin: (body: LoginBodyType) => http.post<LoginResType>('/api/v1/user/login', body),
+  login: (body: LoginBodyType) =>
+    http.post<LoginResType>('/api/auth/login', body, {
+      baseUrl: ''
+    }),
+  sLogout: (
+    body: LogoutBodyType & {
+      accessToken: string
+    }
+  ) =>
+    http.post(
+      '/api/v1/user/logout',
+      {
+        refreshToken: body.refreshToken
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${body.accessToken}`
+        }
+      }
+    ),
+  logout: () => http.post('/api/auth/logout', null, { baseUrl: '' }), // client gọi đến route handler, không cần truyền AT và RT vào body vì AT và RT tự  động gửi thông qua cookie rồi
+  sRefreshToken: (body: RefreshTokenBodyType) => http.post<RefreshTokenResType>('/api/v1/user/refresh-token', body),
+  async refreshToken() {
+    if (this.refreshTokenRequest) {
+      return this.refreshTokenRequest
+    }
+    this.refreshTokenRequest = http.post<RefreshTokenResType>('/api/v1/user/refresh-token', null, {
+      baseUrl: ''
+    })
+    const result = await this.refreshTokenRequest
+    this.refreshTokenRequest = null
+    return result
+  },
+  setTokenToCookie: (body: { accessToken: string; refreshToken: string }) =>
+    http.post('/api/auth/token', body, { baseUrl: '' })
+}
+
+export default authApiRequest
