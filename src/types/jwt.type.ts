c6991e01d69e0fd86db81a9027751797bc087a2d
Ken Ny <kennynguyen1631984@gmail.com>
2024-10-14 17:32:42 +0700

    refactor: Update TokenTypeValue in jwt.type.ts

--- src/types/jwt.type.ts
+++ src/types/jwt.type.ts
@@ -1,16 +1,16 @@
-import { TokenType } from "@/constants/type"
+import { TokenType } from '@/constants/type'
 
 export type TokenTypeValue = (typeof TokenType)[keyof typeof TokenType]
 export interface TokenPayload {
-    user_id: number
-    token_type: TokenTypeValue
-    verify: string
-    exp: number
-    iat: number
+  user_id: number
+  token_type: TokenTypeValue
+  verify: string
+  iat: number
+  exp: number
 }
 
 export interface TableTokenPayload {
-    iat: number
-    number: number
-    token_type: (typeof TokenType)['TableToken']
+  iat: number
+  number: number
+  token_type: (typeof TokenType)['TableToken']
 }
